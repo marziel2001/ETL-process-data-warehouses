b86cdaa17764bf5feae84ebcb14cf8d420fe3d94
Marcel <marziel2001@gmail.com>
2023-12-14 17:15:02 +0100

    added extra driving hour counting

--- ETL_load_studentCourse.sql
+++ ETL_load_studentCourse.sql
@@ -1,14 +1,13 @@
 use szkolaJazdyHD
 go
 
-
 if (object_id('drivingHoursMeasures') is not null) drop view drivingHoursMeasures;
 go
 create view drivingHoursMeasures
 as
 Select
 	dl.FK_StudentCourse,
-	duration = sum(datepart(HOUR, dl.EndTime - dl.StartTime)) -30
+	NoExtraDrivingHours = sum(datepart(HOUR, dl.EndTime - dl.StartTime)) - 30
 
 from szkolaJazdyBD.dbo.DrivingLesson as dl
 group by dl.FK_StudentCourse
@@ -29,7 +28,7 @@ Select
 	carID = 0, -- add car ID
 	sc.FK_Bill,
 	c.BasePrice,
-	noExtraDrivingHours = 0, -- do extra driving hours count
+	noExtraDrivingHours = m.NoExtraDrivingHours, -- do extra driving hours count
 	max(ex.TheoryScore) as TheoryScore
 
 
@@ -37,7 +36,8 @@ from szkolaJazdyBD.dbo.StudentCourse as sc
 inner join szkolaJazdyBD.dbo.Bill as b on sc.FK_Bill = b.ID
 inner join szkolaJazdyBD.dbo.Course as c on sc.FK_Course = c.ID
 inner join szkolaJazdyHD.dbo.vTheory as ex on ex.stcourse = sc.StudentCourse
-group by sc.FK_Student, sc.FK_Course, convert(varchar(10), b.issueTime, 111), convert(varchar(10), b.PaymentTime, 111), sc.FK_Bill, c.BasePrice
+inner join drivingHoursMeasures as m on m.FK_StudentCourse = sc.StudentCourse
+group by sc.FK_Student, sc.FK_Course, convert(varchar(10), b.issueTime, 111), convert(varchar(10), b.PaymentTime, 111), sc.FK_Bill, c.BasePrice, m.NoExtraDrivingHours
 go
 
 select * from vStudentCourse;
