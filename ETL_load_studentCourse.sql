7ce0c76545489bde868f400083d3bbbfb185c220
Marcel <marziel2001@gmail.com>
2023-12-15 03:10:32 +0100

    adding merges

--- ETL_load_studentCourse.sql
+++ ETL_load_studentCourse.sql
@@ -13,9 +13,6 @@ from szkolaJazdyBD.dbo.DrivingLesson as dl
 group by dl.FK_StudentCourse
 go
 
-select * from drivingHoursMeasures
-
-
 if (object_id('vStudentCourse') is not null) drop view vStudentCourse;
 go
 create view vStudentCourse
@@ -25,7 +22,7 @@ Select
 	sc.FK_Course,
 	id.ID as Issue_date,
 	pd.ID as payment_date,
-	carID = 0,					-- add car ID
+	carID = 0,-- add car ID
 	sc.FK_Bill,
 	c.BasePrice,
 	noExtraDrivingHours = m.NoExtraDrivingHours,
@@ -44,6 +41,47 @@ inner join szkolaJazdyHD.dbo.Date as pd on CONVERT(varchar(10), pd.date, 111)
 group by sc.FK_Student, sc.FK_Course, id.ID, pd.ID, sc.FK_Bill, c.BasePrice, m.NoExtraDrivingHours
 go
 
-select * from vStudentCourse;
+merge into szkolaJazdyHD.dbo.StudentCourse as tt
+	using vStudentCourse as st
+		ON st.FK_Student = tt.ID_Student
+		and st.FK_Course = tt.ID_Course
+		and st.Issue_date = tt.ID_BillIssueDate
+		and st.payment_date = tt.ID_BillPaymentDate
+		and st.carID = tt.ID_car
+		and st.FK_Bill = tt.Bill
+		and st.BasePrice = tt.CoursePrice
+		and st.noExtraDrivingHours = No_extra_driving_hours
+		when not matched then insert
+			values (
+				st.FK_Student,
+				st.FK_Course,
+				st.Issue_date,
+				st.payment_date,
+				st.carID,
+				st.FK_Bill,
+				st.BasePrice,
+				st.noExtraDrivingHours
+			);
 
+drop view vStudenCourse
+drop view drivingHoursMeasures
 use master
+
+--CREATE TABLE StudentCourse
+--(
+--    ID INT PRIMARY KEY IDENTITY(1,1),
+--    ID_Student INT NOT NULL,
+--    ID_Course INT NOT NULL,
+--    ID_BillIssueDate INT NOT NULL,
+--    ID_BillPaymentDate INT NOT NULL,
+--	  ID_car INT NOT NULL,
+--    Bill VARCHAR(15) NOT NULL,
+--    CoursePrice MONEY NOT NULL,
+--    No_extra_driving_hours INT NOT NULL,
+--    theory_score FLOAT NOT NULL,
+--    FOREIGN KEY (ID_Student) REFERENCES Student(ID),
+--    FOREIGN KEY (ID_Course) REFERENCES Course(ID),
+--    FOREIGN KEY (ID_BillIssueDate) REFERENCES Date(ID),
+--    FOREIGN KEY (ID_BillPaymentDate) REFERENCES Date(ID),
+--    FOREIGN KEY (ID_car) REFERENCES Car(ID),
+--);
